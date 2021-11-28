247f8304c22931ac0dad49609b2291631adde328
snantony <sonu@comdudes.com>
2021-11-28 16:59:20 +0530

    moved data file

--- node/dist/utils/path.js
+++ node/dist/utils/path.js
@@ -4,4 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = __importDefault(require("path"));
-exports.default = path_1.default.join(__dirname, '../');
+exports.default = path_1.default.join(__dirname, '../', '../');
